6d529b34aa3493afc8a53926f9246d3f015ddf6b
krishnaniyer248 <iyerkrishnan248@gmail.com>
2020-05-26 18:50:41 +0530

    Updated algorithm in comments

--- src/programs/stacks/PrefixToPostfix.java
+++ src/programs/stacks/PrefixToPostfix.java
@@ -37,3 +37,14 @@ public class PrefixToPostfix{
     }
 
 }
+/*
+Algorithm for Prefix to Postfix:
+
+Read the Prefix expression in reverse order (from right to left)
+If the symbol is an operand, then push it onto the Stack
+If the symbol is an operator, then pop two operands from the Stack
+Create a string by concatenating the two operands and the operator after them.
+string = operand1 + operand2 + operator
+And push the resultant string back to Stack
+Repeat the above steps until end of Prefix expression
+ */
